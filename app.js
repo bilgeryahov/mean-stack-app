8568911473b3130853e6e3de6b940befcfb2ccbb
Bilger Yahov <bilger@studyportals.com>
2017-02-12 21:49:28 +0100

    Add first steps with Express. Construct the package json file. Add an index page for the project.

---
+++ app.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Bilger on 12-Feb-17.
+ */
+
+'use strict';
+
+const express = require('express');
+const path    = require('path');
+
+const app     = express();
+
+app.set('port', 3000);
+
+app.get('/', function(req, res){
+
+   console.log('GET the homepage');
+
+   res
+       .status(200)
+       .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res){
+
+    console.log('GET the json');
+
+    res
+        .status(200)
+        .json(
+            {
+                "jsonData": true
+            }
+        );
+});
+
+app.get('/file', function(req, res){
+
+    console.log('GET the file');
+
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'README.md'));
+});
+
+const server = app.listen(app.get('port'), function(){
+
+    let port = server.address().port;
+    console.log('Server listening on port: ', port);
+});
