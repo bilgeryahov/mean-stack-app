54c99fe82ed82ccaa4f2fc736cae800d9b1406a0
Bilger Yahov <bilger@studyportals.com>
2017-02-14 11:45:58 +0100

    Separation of concerns.

--- app.js
+++ app.js
@@ -6,25 +6,21 @@
 
 const express = require('express');
 const path    = require('path');
+const routes  = require('./routes');
 
 const app     = express();
 
 app.set('port', 3000);
 
+app.use(function(req, res, next){
+
+    console.log(req.method, req.url);
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/json', function(req, res){
-
-    console.log('GET the json');
-
-    res
-        .status(200)
-        .json(
-            {
-                "jsonData": true
-            }
-        );
-});
+app.use('/api', routes);
 
 app.get('/file', function(req, res){
 
