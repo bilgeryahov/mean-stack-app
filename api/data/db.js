a4546c7a797a935944390522b8cfb26e225e3e50
Bilger Yahov <bilger@studyportals.com>
2017-03-13 14:25:43 +0100

    New db connection from mongoose.

---
+++ api/data/db.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Bilger on 11-March-17.
+ */
+
+'use strict';
+
+let mongoose = require('mongoose');
+let dburl = 'mongodb://localhost:27017/meanhotel';
+
+let _connection = null;
+
+mongoose.connect(dburl);
+
+mongoose.connection.on('connected', function(){
+
+    console.log('Mongoose connected to ' + dburl);
+    _connection = mongoose.connection;
+});
+
+mongoose.connection.on('disconnected', function(){
+
+    console.log('Mongoose disconnected from ' + dburl);
+});
+
+mongoose.connection.on('error', function(err){
+
+    console.log('Mongoose connection error ' + err);
+});
+
+process.on('SIGINT', function(){
+
+    mongoose.connection.close(function(){
+
+        console.log('Mongoose disconnected through app termination SIGINT');
+        process.exit(0);
+    })
+});
+
+process.on('SIGTERM', function(){
+
+    mongoose.connection.close(function(){
+
+        console.log('Mongoose disconnected through app termination SIGTERM');
+        process.exit(0);
+    })
+});
+
+process.once('SIGUSR2', function(){
+
+    mongoose.connection.close(function(){
+
+        console.log('Mongoose disconnected through app termination SIGUSR2');
+        process.kill(process.pid, 'SIGUSR2');
+    })
+});
+
+
+let get = function(){
+
+    return _connection;
+};
+
+module.exports = {
+    get: get
+};
